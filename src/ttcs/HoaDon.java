7ce79081cd70d2a4655ee745dfc47309f999da8f
Kiet <git@github.com>
2024-01-30 23:03:57 +0700

    create hoadon

---
+++ src/ttcs/HoaDon.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ttcs;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Anh Kiet
+ */
+public class HoaDon {
+    private int soHD;
+    private Date ngayLapHoaDon;
+    private String loaiHD;
+    private String MaNV;
+    private String MaKH;
+    private float ThanhTien;
+    private int TrangThai;
+
+    public HoaDon(){
+        this.TrangThai = 0;
+    }
+
+
+    public int getSoHD() {
+        return soHD;
+    }
+
+    public void setSoHD(int soHD) {
+        this.soHD = soHD;
+    }
+
+    public Date getNgayLapHoaDon() {
+        return ngayLapHoaDon;
+    }
+
+    public void setNgayLapHoaDon(Date ngayLapHoaDon) {
+        this.ngayLapHoaDon = ngayLapHoaDon;
+    }
+
+    public String getLoaiHD() {
+        return loaiHD;
+    }
+
+    public void setLoaiHD(String loaiHD) {
+        this.loaiHD = loaiHD;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public float getThanhTien() {
+        return ThanhTien;
+    }
+
+    public void setThanhTien(float ThanhTien) {
+        this.ThanhTien = ThanhTien;
+    }
+
+    public int getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(int TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+    public String getMaKH() {
+        return MaKH;
+    }
+
+    public void setMaKH(String MaKH) {
+        this.MaKH = MaKH;
+    }
+
+}
