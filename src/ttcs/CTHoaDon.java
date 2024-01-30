a61647a5d8d84147eb324be2772cf31f3ad5053c
Kiet <git@github.com>
2024-01-30 23:22:41 +0700

    create CThoadon, khachhang

---
+++ src/ttcs/CTHoaDon.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ttcs;
+
+/**
+ *
+ * @author Anh Kiet
+ */
+public class CTHoaDon {
+     private String maHoaDon;
+    private String maVatTu;
+    private String tenVatTu;
+    private int soLuong;
+    private int donGia;
+
+
+    public ChiTietHD() {
+    }
+
+    public String getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public void setMaHoaDon(String maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    
+    public String getMaVatTu() {
+        return maVatTu;
+    }
+
+    public void setMaVatTu(String maVatTu) {
+        this.maVatTu = maVatTu;
+    }
+
+    public String getTenVatTu() {
+        return tenVatTu;
+    }
+
+    public void setTenVatTu(String tenVatTu) {
+        this.tenVatTu = tenVatTu;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+}
---
+++ src/ttcs/KhachHang.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ttcs;
+
+/**
+ *
+ * @author Anh Kiet
+ */
+public class KhachHang {
+    private String MaKH;
+    private String TenKH;
+    private String DiaChi;
+    private String Sdt;
+
+    public KhachHang() {
+    }
+
+    public String getMaKH() {
+        return MaKH;
+    }
+
+    public void setMaKH(String MaKH) {
+        this.MaKH = MaKH;
+    }
+
+    public String getTenKH() {
+        return TenKH;
+    }
+
+    public void setTenKH(String TenKH) {
+        this.TenKH = TenKH;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public String getSdt() {
+        return Sdt;
+    }
+
+    public void setSdt(String Sdt) {
+        this.Sdt = Sdt;
+    }
+    
+    
+}
