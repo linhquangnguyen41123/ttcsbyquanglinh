d57d8c18968b3357ae87ea2ebb003a31f29e1f3f
linhquangnguyen41123 <adodareview@gmail.com>
2024-01-31 10:41:59 +0700

    txtxt

--- src/ttcs/nhaSanXuat.java
+++ src/ttcs/nhaSanXuat.java
@@ -2,41 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ttcs;
+package demottcs;
 
 /**
  *
  * @author nguye
  */
 public class nhaSanXuat {
-    private String maNSX;
-    private String tenNSX;
-
-    public nhaSanXuat() {
-    }
-
-    public nhaSanXuat(String maNSX, String tenNSX) {
-        this.maNSX = maNSX;
-        this.tenNSX = tenNSX;
-    }
-
-    public String getMaNSX() {
-        return maNSX;
-    }
-
-    public void setMaNSX(String maNSX) {
-        this.maNSX = maNSX;
-    }
-
-    public String getTenNSX() {
-        return tenNSX;
-    }
-
-    public void setTenNSX(String tenNSX) {
-        this.tenNSX = tenNSX;
-    }
     
-    
-
-
 }
--- src/ttcs/vatTu.java
+++ src/ttcs/vatTu.java
@@ -2,33 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ttcs;
+package demottcs;
 
 /**
  *
  * @author nguye
  */
-
 public class vatTu {
-    private String maVT;
-    private String tenVT;
-    private String phanLoai;
-    private String donViTinh;
-    private String soLuongTon;
-    private String giaBan;
-
-    public vatTu() {
-    }
-
-    public vatTu(String maVT, String tenVT, String phanLoai, String donViTinh, String soLuongTon, String giaBan) {
-        this.maVT = maVT;
-        this.tenVT = tenVT;
-        this.phanLoai = phanLoai;
-        this.donViTinh = donViTinh;
-        this.soLuongTon = soLuongTon;
-        this.giaBan = giaBan;
-    }
-
     
 }
-
