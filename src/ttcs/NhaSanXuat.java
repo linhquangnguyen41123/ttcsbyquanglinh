11d7278ece037448a03bee8d5ca8ab1f48db73f3
Linh ne <adodareview@gmail.com>
2024-02-16 15:07:56 +0700

    linhhihi

---
+++ src/ttcs/NhaSanXuat.java
@@ -0,0 +1,34 @@
+
+package QLVT_BY_LINH;
+
+public class NhaSanXuat {
+    private String MaNSX;
+    private String TenNSX;
+
+    public NhaSanXuat() {
+    }
+
+    public NhaSanXuat(String MaNSX, String TenNSX) {
+        this.MaNSX = MaNSX;
+        this.TenNSX = TenNSX;
+    }
+
+    public String getMaNSX() {
+        return MaNSX;
+    }
+
+    public void setMaNSX(String MaNSX) {
+        this.MaNSX = MaNSX;
+    }
+
+    public String getTenNSX() {
+        return TenNSX;
+    }
+
+    public void setTenNSX(String TenNSX) {
+        this.TenNSX = TenNSX;
+    }
+   
+   
+    
+}
---
+++ src/ttcs/VatTu.java
@@ -0,0 +1,94 @@
+
+package QLVT_BY_LINH;
+
+public class VatTu {
+    private String MaVT;
+    private String TenVT;
+    private String PhanLoai;
+    private String DonViTinh;
+    private int SoLuongTon;
+    private float DonGiaNhap;
+    private float DonGiaBan;
+    private String MaNSX;
+
+    public VatTu() {
+    }
+
+    public VatTu(String MaVT, String TenVT, String PhanLoai, String DonViTinh, int SoLuongTon, float DonGiaNhap, float DonGiaBan, String MaNSX) {
+        this.MaVT = MaVT;
+        this.TenVT = TenVT;
+        this.PhanLoai = PhanLoai;
+        this.DonViTinh = DonViTinh;
+        this.SoLuongTon = SoLuongTon;
+        this.DonGiaNhap = DonGiaNhap;
+        this.DonGiaBan = DonGiaBan;
+        this.MaNSX = MaNSX;
+    }
+
+    public String getMaVT() {
+        return MaVT;
+    }
+
+    public void setMaVT(String MaVT) {
+        this.MaVT = MaVT;
+    }
+
+    public String getTenVT() {
+        return TenVT;
+    }
+
+    public void setTenVT(String TenVT) {
+        this.TenVT = TenVT;
+    }
+
+    public String getPhanLoai() {
+        return PhanLoai;
+    }
+
+    public void setPhanLoai(String PhanLoai) {
+        this.PhanLoai = PhanLoai;
+    }
+
+    public String getDonViTinh() {
+        return DonViTinh;
+    }
+
+    public void setDonViTinh(String DonViTinh) {
+        this.DonViTinh = DonViTinh;
+    }
+
+    public int getSoLuongTon() {
+        return SoLuongTon;
+    }
+
+    public void setSoLuongTon(int SoLuongTon) {
+        this.SoLuongTon = SoLuongTon;
+    }
+
+    public float getDonGiaNhap() {
+        return DonGiaNhap;
+    }
+
+    public void setDonGiaNhap(float DonGiaNhap) {
+        this.DonGiaNhap = DonGiaNhap;
+    }
+
+    public float getDonGiaBan() {
+        return DonGiaBan;
+    }
+
+    public void setDonGiaBan(float DonGiaBan) {
+        this.DonGiaBan = DonGiaBan;
+    }
+
+    public String getMaNSX() {
+        return MaNSX;
+    }
+
+    public void setMaNSX(String MaNSX) {
+        this.MaNSX = MaNSX;
+    }
+
+    
+    
+}
