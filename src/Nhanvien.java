6f6e007480f983874fe3e6fe538845ee8c4b591b
cao-hy <hyngotraidat@gmail.com>
2024-02-17 23:36:45 +0700

    giaodien

---
+++ src/Nhanvien.java
@@ -0,0 +1,94 @@
+package nhanvien;
+import java.util.Date;
+public class Nhanvien {
+    private String maNV;
+    private String ho;
+    private String ten;
+    private boolean gioiTinh;
+    private Date ngaySinh;
+    private String soDT;
+    private String email;
+    public Nhanvien() {
+       
+    }
+    public Nhanvien(String maNV, String ho, String ten, boolean gioiTinh, Date ngaySinh, String soDT, String email) {
+        this.maNV = maNV;
+        this.ho = ho;
+        this.ten = ten;
+        this.gioiTinh = gioiTinh;
+        this.ngaySinh = ngaySinh;
+        this.soDT = soDT;
+        this.email = email;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getSoDT() {
+        return soDT;
+    }
+
+    public void setSoDT(String soDT) {
+        this.soDT = soDT;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "NhanVien{" +
+                "maNV='" + maNV + '\'' +
+                ", ho='" + ho + '\'' +
+                ", ten='" + ten + '\'' +
+                ", gioiTinh=" + gioiTinh +
+                ", ngaySinh=" + ngaySinh +
+                ", soDT='" + soDT + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
